6a3df9783bf8dc83bfab8b46d2c7243058813730
Drin <aldrinisit3196@gmail.com>
2024-01-10 06:59:18 +0800

    Adding navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <h1 className='text-4xl'>Helloiioooo</h1>
+
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import Logo from '../assets/drin-logo.png'
+import { IoMenu, IoClose } from "react-icons/io5"
 
 const Navbar = () => {
   return (
-    <div className='max-w-[1240px]'>
-        <img className='h-10 w-10' src={Logo} alt="" />
+    <div className='max-w-[1240px] items-center mx-auto h-24 p-4 flex justify-between'>
+        <img className='h-9 w-9' src={Logo} alt="" />
 
-        <div className='flex'>
-            <div className='p-3'>Home</div>
-            <div className='p-3'>About me</div>
-            <div className='p-3'>Skills</div>
-            <div className='p-3'>Project</div>
-            <div className='p-3'>Contact</div>
-            <div className='p-3'>Resume</div>
+        <ul className='flex hidden uppercase'>
+          <li className='p-3'>Home</li>
+          <li className='p-3'>About me</li>
+          <li className='p-3'>Skills</li>
+          <li className='p-3'>Project</li>
+          <li className='p-3'>Contact</li>
+        </ul>
+        <div>
+          <IoMenu size={20} />
         </div>
+
+        <div className='fixed left-0 top-0 w-[80%] border bg-amber-300 h-full'>
+        <ul className='pt-4 uppercase'>
+        <img className='h-9 w-9 m-3' src={Logo} alt="" />
+          <li className='p-3'>Home</li>
+          <li className='p-3'>About me</li>
+          <li className='p-3'>Skills</li>
+          <li className='p-3'>Project</li>
+          <li className='p-3'>Contact</li>
+        </ul>
+        </div>
+      
     </div>
   )
 }
