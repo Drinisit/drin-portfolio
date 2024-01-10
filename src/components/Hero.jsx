3c29da2d26f850acdac753144f6f4adbb4fd2972
Drin <aldrinisit3196@gmail.com>
2024-01-10 21:27:23 +0800

    Adding hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Me from '../assets/drin-img.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faFacebook, faInstagram,faGithub,faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 const Hero = () => {
   return (
-    <div>
-
+    <div className='max-w-[1240px] mx-auto items-center p-4'>
+      <div>
+        <p className='uppercase'>Mama mo build</p>
+        <h1 className='text-3xl font-bold '>Hi, I'm Aldrin Isit</h1>
+        <p>An aspiring Web Developer who specialized on Front-End base in Pangasinan, Philippines</p>
+        <button className='bg-amber-500 border border-amber-500 p-2 my-2'>About me</button> 
+        <button className='border border-amber-500 p-2 mx-2'>Resume</button>
+        <div>
+          <FontAwesomeIcon className='text-xl mt-1' icon={faFacebook}/>
+          <FontAwesomeIcon className='text-xl mt-1 mx-3' icon={faInstagram}/>
+          <FontAwesomeIcon className='text-xl mt-1 mx-2' icon={faGithub}/>
+          <FontAwesomeIcon className='text-xl mt-1 mx-2' icon={faLinkedin}/>
+        </div>
+      </div>
+        <img src={Me} alt="" />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className='max-w-[1240px] items-center mx-auto h-24 p-3 flex justify-between'>
+    <div className='max-w-[1240px] items-center mx-auto h-24 p-4 flex justify-between'>
         <img className='h-9 w-9' src={Logo} alt="" />
 
         <ul className='hidden md:flex uppercase'>
@@ -28,11 +28,11 @@ const Navbar = () => {
         <div  className={!nav ? 'fixed left-0 top-0 w-[90%] border bg-amber-400 h-full ease-in-out duration-500' : 'fixed right-[100%]' }>
           <ul className='pt-4 uppercase'>
           <img className='h-9 w-9 m-3' src={Logo} alt="" />
-            <li className='p-3 border-b border-amber-500'>Home</li>
-            <li className='p-3 border-b border-amber-500'>About me</li>
-            <li className='p-3 border-b border-amber-500'>Skills</li>
-            <li className='p-3 border-b border-amber-500'>Project</li>
-            <li className='p-3'>Contact</li>
+            <li className='p-4 border-b border-amber-500'>Home</li>
+            <li className='p-4 border-b border-amber-500'>About me</li>
+            <li className='p-4 border-b border-amber-500'>Skills</li>
+            <li className='p-4 border-b border-amber-500'>Project</li>
+            <li className='p-4'>Contact</li>
           </ul>
         </div>
       
