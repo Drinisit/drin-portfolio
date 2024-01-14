94258b36444b79007d9d8a44fbf010fd72a290a6
Drin <aldrinisit3196@gmail.com>
2024-01-14 18:21:20 +0800

    Working on project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,10 +13,10 @@ const Project = () => {
                     </div>
                 </div>
 
-                <div className='w-full flex flex-col md:col-span-2'>
+                <div className='w-full flex flex-col md:col-span-3'>
                     <div>
                         <h2>L-nu Library Website</h2>
-                        <p>The university library website is a vital online resource catering to students and faculty seeking efficient access to the institution's extensive library holdings. This platform offers an intuitive search interface, enabling users to effortlessly locate books, research materials, and academic resources. Whether students are searching by title, author, subject, or keyword, the website streamlines their quest for information, enhancing their research and learning experiences. Furthermore, the site provides real-time availability status for books, empowering users to check item availability, renew borrowed materials, reserve items, and request interlibrary loans when needed. With additional features like curated lists and digital resource access, the university library website is an indispensable tool for academic success.</p>
+                        <p className='bg-yellow-100 lg:w-2/3'>The university library website is a vital online resource catering to students and faculty seeking efficient access to the institution's extensive library holdings. This platform offers an intuitive search interface, enabling users to effortlessly locate books, research materials, and academic resources. Whether students are searching by title, author, subject, or keyword, the website streamlines their quest for information, enhancing their research and learning experiences. Furthermore, the site provides real-time availability status for books, empowering users to check item availability, renew borrowed materials, reserve items, and request interlibrary loans when needed. With additional features like curated lists and digital resource access, the university library website is an indispensable tool for academic success.</p>
                     </div>
                 </div>
 
