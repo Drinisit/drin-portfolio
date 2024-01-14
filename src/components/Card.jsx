c96abc653dc3e040ecd67a7a4f2632ff8f6b1f82
Drin <aldrinisit3196@gmail.com>
2024-01-14 19:45:02 +0800

    2 Rows on xxs screen cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,14 +7,14 @@ const Card = () => {
   return (
     <div className='w-full py-11 px-6'>
       <div className='max-w-[1240px] mx-auto'>
-        <div className='p-4 grid gap-4 xs:px-16 xs:grid-cols-2 md:px-28 md:grid-cols-3 md:gap-6 lg:px-48 '>
-          <h1 className='text-3xl font-bold uppercase text-center xs:col-span-2 md:col-span-3'>Skills</h1>
-            <div className='h-10 bg-amber-400 xs:h-auto xs:square'><FaHtml5 size={70} className='mx-auto' /></div>
-            <div className='h-10 bg-orange-400  xs:h-auto xs:square'><FaCss3Alt size={70} className='mx-auto' /></div>
-            <div className='h-10 bg-amber-400 xs:h-auto xs:square'><DiJavascript size={70} className='mx-auto' /></div>
-            <div className='h-10 bg-orange-400  xs:h-auto xs:square'><FaReact size={70} className='mx-auto' /></div>
-            <div className='h-10 bg-amber-400 xs:h-auto xs:square'><SiTailwindcss size={70} className='mx-auto' /></div>
-            <div className='h-10 bg-orange-400  xs:h-auto xs:square'><FaGithub size={70} className='mx-auto' /></div>
+        <div className='p-4 grid gap-4 xxs:px-16 xxs:grid-cols-2 md:px-28 md:grid-cols-3 md:gap-6 lg:px-48 '>
+          <h1 className='text-3xl font-bold uppercase text-center xxs:col-span-2 md:col-span-3'>Skills</h1>
+            <div className='h-10 bg-amber-400 xxs:h-auto xs:square'><FaHtml5 size={70} className='mx-auto' /></div>
+            <div className='h-10 bg-orange-400  xxs:h-auto xs:square'><FaCss3Alt size={70} className='mx-auto' /></div>
+            <div className='h-10 bg-amber-400 xxs:h-auto xs:square'><DiJavascript size={70} className='mx-auto' /></div>
+            <div className='h-10 bg-orange-400  xxs:h-auto xs:square'><FaReact size={70} className='mx-auto' /></div>
+            <div className='h-10 bg-amber-400 xxs:h-auto xs:square'><SiTailwindcss size={70} className='mx-auto' /></div>
+            <div className='h-10 bg-orange-400  xxs:h-auto xs:square'><FaGithub size={70} className='mx-auto' /></div>
        </div>
 
       </div>
