a00437576adfdea080cc67590b004e1186e761a1
Drin <aldrinisit3196@gmail.com>
2024-01-16 14:13:14 +0800

    Adding image on project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,36 +1,41 @@
 import React from 'react'
+import Img1 from '../assets/lnu-lib.jpg'
+import Img2 from '../assets/pizzasap.jpg'
+import Img3 from '../assets/manhwasite.jpg'
+import Img4 from '../assets/shoetha.jpg'
 
 const Project = () => {
   return (
-    <div className='w-full py-11 px-6'>
-        <div className='max-w-[1240px] mx-auto grid grid-cols-4 gap-8'>
-            <div className='w-full flex flex-col md:col-span-4'>
-                <h1 className='text-3xl font-bold uppercase'>Projects</h1>
+    <div className='w-full py-11 px-4'>
+        <div className='max-w-[1240px] mx-auto grid gap-4 grid-cols-4'>
+            <div className='w-full flex flex-col col-span-4'>
+                <h1 className='text-3xl font-bold uppercase text-center'>Project</h1>
+            </div>
+            
+
+            <div>
+                <img src={Img1} alt="" />
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
             </div>
 
-                <div className='w-full flex flex-col'>
-                    <div className='w-36 h-36 bg-amber-500'>
-                    </div>
-                </div>
+            <div>
+                <img src={Img2} alt="" />
+                <h2>Title</h2>
+                <p>Iure ratione, iusto ipsum deserunt nulla id repellendus vitae dolorum eos suscipit fugiat veniam ullam.</p>
+            </div>
 
-                <div className='w-full flex flex-col md:col-span-3'>
-                    <div>
-                        <h2>L-nu Library Website</h2>
-                        <p className='bg-yellow-100 lg:w-2/3'>The university library website is a vital online resource catering to students and faculty seeking efficient access to the institution's extensive library holdings. This platform offers an intuitive search interface, enabling users to effortlessly locate books, research materials, and academic resources. Whether students are searching by title, author, subject, or keyword, the website streamlines their quest for information, enhancing their research and learning experiences. Furthermore, the site provides real-time availability status for books, empowering users to check item availability, renew borrowed materials, reserve items, and request interlibrary loans when needed. With additional features like curated lists and digital resource access, the university library website is an indispensable tool for academic success.</p>
-                    </div>
-                </div>
+            <div>
+                <img src={Img3} alt="" />
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
 
-                <div className='w-full flex flex-col'>
-                    <div>
-                        <h2>Pizzarap</h2>
-                        <p>The university library website is a vital online resource catering to students and faculty seeking efficient access to the institution's extensive library holdings. This platform offers an intuitive search interface, enabling users to effortlessly locate books, research materials, and academic resources. Whether students are searching by title, author, subject, or keyword, the website streamlines their quest for information, enhancing their research and learning experiences. Furthermore, the site provides real-time availability status for books, empowering users to check item availability, renew borrowed materials, reserve items, and request interlibrary loans when needed. With additional features like curated lists and digital resource access, the university library website is an indispensable tool for academic success.</p>
-                    </div>
-                </div>
-
-                <div className='w-full flex flex-col'>
-                    <div className='w-36 h-36 bg-amber-500'>
-                    </div>
-                </div>
+            <div>
+                <img src={Img4} alt="" />
+                <h2>Title</h2>
+                <p>Hic unde amet nisi nobis enim deleniti at vero reprehenderit culpa voluptatum dolor nemo nam fuga eum.</p>
+            </div>
 
         </div>
 
